a316e3021177ce6409f5a96a4d80c9af045f6208
luii <czarnetzki.philipp@protonmail.ch>
2019-09-10 18:57:24 +0200

    add documentation

--- src/app/components/cah-scoreboard/cah-scoreboard-list/cah-scoreboard-list.component.ts
+++ src/app/components/cah-scoreboard/cah-scoreboard-list/cah-scoreboard-list.component.ts
@@ -1,19 +1,32 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Player} from '../../../interfaces/player';
 
+/**
+ * The scoreboard list class
+ * @access public
+ * @class
+ * @export
+ * @name CahScoreboardListComponent
+ */
 @Component({
   selector: 'cah-scoreboard-list',
   templateUrl: './cah-scoreboard-list.component.html',
   styleUrls: ['./cah-scoreboard-list.component.scss']
 })
-export class CahScoreboardListComponent implements OnInit {
+export class CahScoreboardListComponent {
 
+  /**
+   * Player input property that display the currently playing users in the list
+   * @access   public
+   * @property {Player[]} players
+   */
   @Input()
   public players: Player[];
 
+  /**
+   * Assigns the defaults
+   * @access public
+   * @constructor
+   */
   public constructor() { }
-
-  ngOnInit() {
-  }
-
 }
