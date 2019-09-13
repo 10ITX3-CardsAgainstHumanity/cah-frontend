3070409fc00b42ad6971d46daef4260ab88b32bd
luii <czarnetzki.philipp@protonmail.ch>
2019-09-13 22:19:17 +0200

    add cah-title-screen module

---
+++ src/app/modules/cah-title-screen/cah-title-screen.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CahTitleScreenComponent } from './cah-title-screen.component';
+
+describe('CahTitleScreenComponent', () => {
+  let component: CahTitleScreenComponent;
+  let fixture: ComponentFixture<CahTitleScreenComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CahTitleScreenComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CahTitleScreenComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/cah-title-screen/cah-title-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cah-title-screen',
+  templateUrl: './cah-title-screen.component.html',
+  styleUrls: ['./cah-title-screen.component.scss']
+})
+export class CahTitleScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
