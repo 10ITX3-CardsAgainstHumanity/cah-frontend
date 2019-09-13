9bf15cf142d69469b9c66b7e60f5940197e0ed27
luii <czarnetzki.philipp@protonmail.ch>
2019-09-13 22:16:21 +0200

    remove unused modules and add a root routing module

---
+++ src/app/app-routing.component.ts
@@ -0,0 +1,14 @@
+import {RouterModule} from '@angular/router';
+import {NgModule} from '@angular/core';
+
+const routes = [
+  {path: '', loadChildren: './modules/cah-title-screen/cah-title-screen.module#CahTitleScreenModule'},
+  {path: 'game/room', loadChildren: './modules/cah-game-room/cah-game-room.module#CahGameRoomModule'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import {CahBlackCardModule} from './components/cah-black-card/cah-black-card.module';
-import {CahWhiteCardModule} from './components/cah-white-card/cah-white-card.module';
-import {CahScoreboardModule} from './components/cah-scoreboard/cah-scoreboard.module';
-import { CahPlayerHandComponent } from './components/cah-player-hand/cah-player-hand.component';
-import { CahPlayerComponent } from './components/cah-player/cah-player.component';
+import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { CahSelectedCardsComponent } from './components/cah-selected-cards/cah-selected-cards.component';
+import {AppRoutingModule} from './app-routing.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CahPlayerHandComponent,
-    CahPlayerComponent,
-    CahSelectedCardsComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    CahBlackCardModule,
-    CahWhiteCardModule,
-    CahScoreboardModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
