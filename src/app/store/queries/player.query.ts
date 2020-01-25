6a3db9e0f92212e2da6d7d148a19f47cf5b9f67b
luii <czarnetzki.philipp@protonmail.ch>
2020-01-25 17:55:04 +0100

    chore(player query): remove obsolete code

--- src/app/store/queries/player.query.ts
+++ src/app/store/queries/player.query.ts
@@ -4,7 +4,6 @@ import {PlayerState} from '@store/states/player.state';
 import {PlayerStore} from '@store/player.store';
 import {Player} from '@shared/models/player.model';
 import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
 import {WhiteCard} from '@shared/models/white-card.model';
 
 /**
@@ -20,39 +19,12 @@ import {WhiteCard} from '@shared/models/white-card.model';
 export class PlayerQuery extends QueryEntity<PlayerState, Player> {
 
   /**
-   * The local player that we're controlling right now
-   * @access public
-   * @property {Observable<Player>} localPlayer$
-   */
-  public localPlayer$: Observable<Player>;
-
-  /**
    * The local players cards observable
    * @access   public
    * @property {Observable<WhiteCard[]>} localPlayerCards$
    */
   public localPlayerCards$: Observable<WhiteCard[]>;
 
-  /**
-   * Selects the leading player based on the points he has
-   * @access public
-   * @property {Observable<Player>} leadingPlayer$
-   */
-  public leadingPlayer$: Observable<Player>;
-
-  /**
-   * Selects the czar of the current round
-   * @access public
-   * @property {Observable<Player>} czarPlayer$
-   */
-  public czarPlayer$: Observable<Player>;
-
-  /**
-   * Selects the loading state of the store
-   * @access   public
-   * @property {Observable<boolean>} isLoading$
-   */
-  public isLoading$: Observable<boolean>;
 
   /**
    * Assigns the defaults
@@ -62,10 +34,6 @@ export class PlayerQuery extends QueryEntity<PlayerState, Player> {
   public constructor(protected readonly _store: PlayerStore) {
     super(_store);
 
-    this.isLoading$        = this.selectLoading();
-    this.localPlayer$      = this.selectActive();
     this.localPlayerCards$ = this.selectActive(entity => entity.cards);
-    this.leadingPlayer$    = this.selectAll().pipe(map((players: Player[]) => players[0]));
-    this.czarPlayer$       = this.selectAll().pipe(map((players: Player[]) => players.find((player: Player) => player.isCzar === true)));
   }
 }
