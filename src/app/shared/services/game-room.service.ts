4de64d3de189ee9836a6397b4515675befc30a58
luii <czarnetzki.philipp@protonmail.ch>
2020-01-25 18:00:38 +0100

    chore(gameroom service): remove obsolete code

--- src/app/shared/services/game-room.service.ts
+++ src/app/shared/services/game-room.service.ts
@@ -33,9 +33,7 @@ export enum GameState {
  * @export
  * @name GameRoomService
  */
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class GameRoomService {
 
   /**
@@ -58,6 +56,13 @@ export class GameRoomService {
    * @property {Observable<ResponseMessage>} $onRoomJoined
    */
   public $onRoomJoined: Observable<ResponseMessage> = this._socket.fromEvent('game.join');
+
+  /**
+   * Observable state of the players that are currently in the room
+   * @access   public
+   * @property {Observable<ResponseMessage>} $onRoomPlayers
+   */
+  public $onRoomPlayers: Observable<ResponseMessage> = this._socket.fromEvent('game.players');
 
   /**
    * Assigns the defaults
@@ -98,222 +103,20 @@ export class GameRoomService {
   }
 
   /**
-   * Requests the game room states from the backend
+   * Leaves the current game
    * @access public
    * @return {void}
    */
-  public requestGameState(): void {
-    this.onGameStateResponse().subscribe();
-    this._socket.emit('game.room.request.states');
+  public leaveGameRoom(): void {
+    this._socket.emit('game.leave');
   }
 
   /**
-   * Listens for the game states request response
-   * @access public
-   * @return {Observable<GameRoomState>}
-   */
-  public onGameStateResponse(): Observable<GameRoomState> {
-    return this._socket.fromEvent('game.room.response.states')
-      .pipe(tap((state: GameRoomState) => this._gameRoomStore.update(state)));
-  }
-
-  /**
-   * Gets triggered if a user left the game room
-   * so we can update our players accordingly
-   * @access public
-   * @return {Observable<Player[]>}
-   */
-  public onPlayerLeave(): Observable<Player[]> {
-    return this._socket.fromEvent('game.room.player.leaved')
-      // .pipe(tap((players: Player[]) => this._gameRoomStore.update({players})));
-  }
-
-  /**
-   * Emits the start event for the game
+   * Requests all players that are currently in the room
    * @access public
    * @return {void}
    */
-  public startGame(): void {
-    this._socket.emit('game.room.start');
-  }
-
-  /**
-   * Gets triggered when the game was started
-   * @access public
-   * @return {Observable<object>}
-   */
-  public onStartGame(): Observable<object> {
-    return this._socket.fromEvent('game.room.started')
-      // .pipe(tap(() => this._gameRoomStore.update({ started: true })));
-  }
-
-  /**
-   * Gets triggered when the game room is full
-   */
-  public onRoomFull(): Observable<object> {
-    return this._socket.fromEvent('game.room.full')
-      // .pipe(tap(() => this._gameRoomStore.update({ roomIsFull: true, started: true })));
-  }
-
-  /**
-   * Requests a set of cards from the socket endpoint
-   * @access public
-   * @param  {number} amount
-   * @return {void}
-   */
-  public requestPlayerCards(amount: number): void {
-    this._socket.emit('game.room.request.cards', {amount});
-  }
-
-  /**
-   * Gets triggered when the response from the request player cards
-   * comes back to us and gives us a array of cards that get added to
-   * the end of our already populated array of white cards
-   * @access public
-   * @return {void}
-   */
-  public onPlayerRequestCards(): Observable<WhiteCard[]> {
-    return this._socket.fromEvent('game.room.response.cards')
-      // .pipe(tap((cards: WhiteCard[]) => this._gameRoomStore.update({cards})));
-  }
-
-  /**
-   * Emits a event to inform the backend that were ready to play
-   * @access public
-   * @param  {string} username
-   * @return {void}
-   */
-  public playerIsReady(username): void {
-    this._socket.emit('game.room.player.ready', {username});
-  }
-
-  /**
-   * Gets triggered if a player is ready
-   * and carries the user that is ready
-   * @access public
-   * @return {Observable<Player>}
-   */
-  public onPlayerReady(): Observable<Player> {
-    return this._socket.fromEvent('game.room.player.isReady');
-      // .pipe(tap()) set the player in the store to ready
-  }
-
-  /**
-   * Gets triggered if all players are ready
-   * @access public
-   * @return {Observable<Player[]>}
-   */
-  public onAllPlayersReady(): Observable<Player[]> {
-    return this._socket.fromEvent('game.room.players.ready');
-  }
-
-  /**
-   * Gets triggered if the next czar got selected
-   * @access public
-   * @return {Observable<Player>}
-   */
-  public onNextCzarSelected(): Observable<Player> {
-    return this._socket.fromEvent('game.room.nextCzar');
-      // set czar on player in store
-  }
-
-  /**
-   * Requests a black card from the backend
-   * @access public
-   * @return {void}
-   */
-  public requestBlackCard(): void {
-    this._socket.emit('game.room.request.blackCard');
-  }
-
-  /**
-   * Gets triggered when the black card request was issued
-   * and carries a black card in its body
-   * @access public
-   * @return {Observable<BlackCard>}
-   */
-  public onBlackCardResponse(): Observable<BlackCard> {
-    return this._socket.fromEvent('game.room.response.blackCard');
-  }
-
-  /**
-   * Gets triggered when the backend is ready to accept white cards
-   * @access public
-   * @return {Observable<void>}
-   */
-  public onAcceptingWhiteCards(): Observable<object> {
-    return this._socket.fromEvent('game.room.accepting.whiteCards')
-      .pipe(tap(() => {
-        this._socket.emit('game.room.accepting.whiteCards.ack');
-      }));
-  }
-
-  /**
-   * Gets emitted when all necessary white cards were selected by the user
-   * @access public
-   * @param  {WhiteCard[]} cards
-   * @return {void}
-   */
-  public whiteCardsSelected(cards: WhiteCard[]): void {
-    this._socket.emit('game.room.whiteCards.selected', {cards});
-  }
-
-  /**
-   * Gets triggered when the player wasnt fast enough to select its cards
-   * and the server skips him
-   * @access public
-   * @return {Observable<object>}
-   */
-  public onRountTimeout(): Observable<object> {
-    return this._socket.fromEvent('game.room.round.timeout');
-  }
-
-  /**
-   * Gets triggered when all players have submitted they're
-   * white cards and the server responds with this event
-   * @access public
-   * @return {Observable<object>}
-   */
-  public onAllWhiteCardsSelected(): Observable<object> {
-    return this._socket.fromEvent('game.room.allWhiteCards.selected');
-  }
-
-  /**
-   * Emits a event that chooses the black card
-   * @access public
-   * @param {BlackCard} card
-   */
-  public czarPickBlackCard(card: BlackCard): void {
-    this._socket.emit('game.room.blackCard.picked', {card});
-  }
-
-  /**
-   * Gets triggered when the czar picked his favourite cards
-   * @access public
-   * @return {Observable<object>}
-   */
-  public onCzarPickedCards(): Observable<object> {
-    return this._socket.fromEvent('game.room.whiteCards.picked');
-  }
-
-  /**
-   * Gets triggered when the scoreboard needs to be updated
-   * @access public
-   * @return {Observable<object>}
-   */
-  public onUpdateScoreboard(): Observable<object> {
-    return this._socket.fromEvent('game.room.update.scoreboard')
-      .pipe(tap(() => {
-        this._socket.emit('game.room.update.scoreboard.ack');
-      }));
-  }
-
-  /**
-   * Gets triggered when the next round is about to start
-   * @access public
-   * @return {Observable<object>}
-   */
-  public onRoundStartingIn(): Observable<object> {
-    return this._socket.fromEvent('game.room.round.startingIn');
+  public requestPlayers(): void {
+    this._socket.emit('game.players');
   }
 }
