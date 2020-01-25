c7cd7c5fcf85e1965b81adbdd678806bea08c360
luii <czarnetzki.philipp@protonmail.ch>
2020-01-25 17:58:41 +0100

    refactor(cah-room-badge component): use inputs for modularity

--- src/app/shared/components/cah-room-badge/cah-room-badge.component.ts
+++ src/app/shared/components/cah-room-badge/cah-room-badge.component.ts
@@ -1,8 +1,4 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, ParamMap} from '@angular/router';
-import {FormBuilder, FormGroup} from '@angular/forms';
-import {GameRoomService} from '@services/game-room.service';
-import {GameRoomQuery} from '@store/queries/game-room.query';
+import {Component, Input} from '@angular/core';
 import {Observable} from 'rxjs';
 
 @Component({
@@ -17,19 +13,14 @@ export class CahRoomBadgeComponent {
    * @access   public
    * @property {Observable<string>}
    */
+  @Input()
   public roomId$: Observable<string>;
 
   /**
    * Assigns the defaults
    * @access public
-   * @param  {ActivatedRoute}  _route
-   * @param  {GameRoomQuery}   _gameRoomQuery
-   * @param  {GameRoomService} _gameRoomService
    * @constructor
    */
-  public constructor(private readonly _route:           ActivatedRoute,
-                     private readonly _gameRoomQuery:   GameRoomQuery,
-                     private readonly _gameRoomService: GameRoomService) {
-    this.roomId$ = this._gameRoomQuery.roomId$;
+  public constructor() {
   }
 }
