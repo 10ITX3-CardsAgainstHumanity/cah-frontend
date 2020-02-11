6be3520a80000981f90ea9c0b55dbbdc7199441a
luii <czarnetzki.philipp@protonmail.ch>
2020-02-11 19:20:53 +0100

    chore(core module): import modules that will be loaded only once on the application

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,35 +1,24 @@
 import {NgModule} from '@angular/core';
-
-import {CoreRoutingModule} from './core-routing.module';
-import {CoreRootComponent} from './components/core-root/core-root.component';
-import {SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
-import {environment} from '@env/environment';
+import {CommonModule} from '@angular/common';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SocketIoModule} from 'ngx-socket-io';
+import {environment} from '@env/environment';
 import {AkitaNgDevtools} from '@datorama/akita-ngdevtools';
-import {CahJoinGameDialogModule} from '@shared/components/cah-join-game-dialog/cah-join-game-dialog.module';
-import {CahNewGameDialogModule} from '@shared/components/cah-new-game-dialog/cah-new-game-dialog.module';
-import {BrowserModule} from '@angular/platform-browser';
-import {CahJoinGameDialogComponent} from '@shared/components/cah-join-game-dialog/cah-join-game-dialog.component';
-import {CahNewGameDialogComponent} from '@shared/components/cah-new-game-dialog/cah-new-game-dialog.component';
 import {MatSnackBarModule} from '@angular/material';
-
-const ioConfig: SocketIoConfig = {url: environment.io.url, options: {}};
+import {CahJoinGameDialogModule} from '@app/core/components/cah-join-game-dialog/cah-join-game-dialog.module';
+import {CahNewGameDialogModule} from '@app/core/components/cah-new-game-dialog/cah-new-game-dialog.module';
 
 @NgModule({
-  declarations: [CoreRootComponent],
+  declarations: [],
   imports: [
-    BrowserModule,
-    MatSnackBarModule,
-    CoreRoutingModule,
+    CommonModule,
     FontAwesomeModule,
-    BrowserAnimationsModule,
-    environment.production ? [] : [AkitaNgDevtools.forRoot()],
-    SocketIoModule.forRoot(ioConfig),
+    MatSnackBarModule,
+    CahNewGameDialogModule,
     CahJoinGameDialogModule,
-    CahNewGameDialogModule
-  ],
-  entryComponents: [CahJoinGameDialogComponent, CahNewGameDialogComponent],
-  bootstrap: [CoreRootComponent]
+    environment.production ? [] : [AkitaNgDevtools.forRoot()],
+    SocketIoModule.forRoot({url: environment.io.url, options: {}}),
+  ]
 })
-export class CoreModule {}
+export class CoreModule {
+}
