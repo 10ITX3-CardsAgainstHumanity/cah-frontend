d9a3ab7861c16681bf16ca4c8d76b7d0a6ebf913
luii <czarnetzki.philipp@protonmail.ch>
2020-02-16 20:58:38 +0100

    refactor(cah-game-room): update game room component

--- src/app/modules/cah-game-room/cah-game-room.module.ts
+++ src/app/modules/cah-game-room/cah-game-room.module.ts
@@ -4,10 +4,10 @@ import {RouterModule, Routes} from '@angular/router';
 import {CahGameRoomComponent} from './cah-game-room.component';
 import {SharedModule} from '@shared/shared.module';
 import {MatButtonModule} from '@angular/material';
-import {CahPlayerModule} from '@app/core/components/cah-player/cah-player.module';
 import {CahRoomBadgeModule} from '@app/core/components/cah-room-badge/cah-room-badge.module';
 import {CahScoreboardModule} from '@app/core/components/cah-scoreboard/cah-scoreboard.module';
 import {CahSelectedCardsModule} from '@app/core/components/cah-selected-cards/cah-selected-cards.module';
+import {CahPlayerHandModule} from '@app/core/components/cah-player-hand/cah-player-hand.module';
 
 const routes: Routes = [
   {path: '', component: CahGameRoomComponent}
@@ -15,16 +15,16 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [CahGameRoomComponent],
-    imports: [
-        CommonModule,
-        SharedModule,
-        CahPlayerModule,
-        CahRoomBadgeModule,
-        CahScoreboardModule,
-        CahSelectedCardsModule,
-        RouterModule.forChild(routes),
-        MatButtonModule,
-    ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    CahRoomBadgeModule,
+    CahScoreboardModule,
+    CahSelectedCardsModule,
+    RouterModule.forChild(routes),
+    MatButtonModule,
+    CahPlayerHandModule,
+  ],
   exports: [
     RouterModule
   ]
